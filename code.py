ddcb89644d6b9913e690877166ad3ced7a968b2c
serafimjp <72172563+serafimjp@users.noreply.github.com>
2020-09-30 20:48:30 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+# função print
+
+print('Olá, Mundo!')
+print(2020)
+print(1 + 1)
+
+# operações matemáticas (toda divisão gera um número decimal (conhecidos como float ou números com ponto flutuante)
+
+1 + 1
+10 - 5
+2 + 2 - 1
+-2 + 2
+-2 + 4
+2 * 2
+2 * 2.0
+10 / 2
+10 / 2.0
